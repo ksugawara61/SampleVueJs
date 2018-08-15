e2c8111863eb54fdfabd9771862e14b3cf7b319b
ksugawara61 <katsuya@kokohadoko.net>
2018-08-15 12:23:16 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+
+    // two way data binding (to UI)
+
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            newItem: '',
+            todos: []
+            /*[{
+                title:  'task 1',
+                isDone: false
+            }, {
+                title: 'task 2',
+                isDone: false
+            }, {
+                title: 'task 3',
+                isDone: true
+            }]*/
+        },
+        methods: {
+            addItem: function() {
+                var item = {
+                    title: this.newItem,
+                    isDone: false
+                };
+                this.todos.push(item);
+                this.newItem = '';
+            },
+            deleteItem: function(index) {
+                if (confirm('are you sure?')) {
+                    this.todos.splice(index, 1);
+                }
+            }
+        },
+        computed: {
+            remaining: function() {
+                var items = this.todos.filter(function(todo) {
+                    return !todo.isDone;
+                });
+                return items.length;
+            }
+        }
+    });
+
+})();
+
